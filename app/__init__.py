20314133b93ad5e84c14321e7ea51f3b94e4697c
Xavier Moreno <xavierjmoreno401@gmail.com>
2023-02-05 03:34:12 -0600

    Translated Santiago's Hobby Section and added the appropriate routes

--- app/__init__.py
+++ app/__init__.py
@@ -37,7 +37,8 @@ xavier_career = {
     "jobtitle": ["Site Reliability Engineer Fellow",  "Noc Analyst", "Service Desk Analyst",  "Private Tutor"],
     "dates": ["Jan 2023 - April 2023", "Nov 2022 - Present", "March 2021 - Nov 2021", "Nov 2019 - Present"],
     "descriptions": {
-        0: ["Worked in a cohort of 20 other members"],
+        0: ["Worked in a cohort of 20 other members working on various Open Source Projects in Siter Reliability Engineering",
+            "Projects ranged from creating a personal portfolio website to working on containers, Unit-Testing, CI/CD, monitoring and networking."],
         1: ["Performed continuous monitoring & incident response of various systems through Grafana, escalating tickets when required using ServiceNow. Coordinated with various teams from departments on incidents",
             "Continuously updated & monitored 250 RHEL 7 machines and 500 Windows 10 machines",
             "Performed hardware and software upgrades for machines running AVTEC software"],
@@ -52,13 +53,20 @@ xavier_career = {
     }
 }
 
+xavier_hobby = {
+    "hobbies": ["Hiking", "Language Learning"],
+    "description": ["In my opinion hiking is one of the best things you can do to relax yourself, especially when you are high on stress. Pictured to the right is a photo I took while on a hike in the Grand Canyon. Most of the hiking I do isn't as crazy as that, although my trip was a good memory. Generally just going walking among grassy and slightly rocky fields is my go to!", "I find studying languages to be very fascinating. There have been many studies done on the effects on language, and the results are very interesting. To study language is to quite literally change your brain and perspective. Well benefits aside, I simply enjoy taking on different systems of writing/speaking and thought. I'm currently studying Japanese, and so far it's been quite a journey. Kanji is definitely no joke!"],
+    "url": ['./static/img/xav_hobby/xav_hobby1.jpg', './static/img/xav_hobby/xav_hobby2.jpg'],
+    "credit": ['', 'credit :aaron-burden unsplash ']
+}
+
 xavier_mapper = {
     "country_origin": "United States of America, TX, Dallas",
     "hometown": "Dallas, TX",
     "homecity": "Dallas",
     "hometown_coords": {"lat": 32.7767, "long": -96.7970},
     "places": ['Phoenix, AZ - Grand Canyon', 'Austin, TX - UT', 'San Antonio,  TX - UTSA', 'San Diego, CA - MCRD', 'Cambridge, MA - MIT', 'Cambridge, MA - Harvard', 'Stanford, CA - Stanford'],
-    "description": ['My visit to the grand canyon is easily one of my favorite memories. Enjoying time with friends in new places is always welcome.', 'My first time going to another city for a hackathon. This is where I participated at HackTX!', 'After HackTX in Austin, I ended up starting to travel further and further for hackathons. This is RowdyHacks.', 'This the first time I ever flew within the US. I saw one of my close relatives graduate at San Diego\'s Marine Bootcamp', 'I participated at HackMIT here. It was a legendary experience, and my first time flying for a hackathon!', 'In Boston again! This time as a Judge for HackHarvard.', 'One of the most exciting hackathons yet. Stanfords TreeHacks!'],
+    "description": ['My visit to the grand canyon is easily one of my favorite memories. Enjoying time with friends in new places is always welcome.', 'My first time going to another city for a hackathon. This is where I participated at HackTX!', 'After HackTX in Austin, I ended up starting to travel further and further for hackathons. This is RowdyHacks.', 'This the first time I ever flew within the US. I saw one of my close relatives graduate at San Diego\'s Marine Bootcamp', 'I participated at HackMIT here. It was a legendary experience, and my first time flying for a hackathon!', 'In Boston again! This time as a Judge for HackHarvard.', 'One of the most exciting hackathons yet. Stanford\'s TreeHacks!'],
     "places_coords": [
         {"lat": 36.0544, "long": -112.1401},
         {"lat": 30.2849, "long": -97.7341},
@@ -161,6 +169,7 @@ def xav_hobby():
                            hobby_route='xav_hobby',
                            education_route='xav_education',
                            places_route='xav_places',
+                           user_hobbies=xavier_hobby,
                            url=os.getenv("URL"))
 
 
