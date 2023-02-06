6bf3fd0f57584506cd17279ca8d2ff34a678ed07
Xavier Moreno <xavierjmoreno401@gmail.com>
2023-02-06 02:44:29 -0600

    successfully stored all the user data into the users.py file

--- app/python_helpers/profile_sections.py
+++
@@ -1,1 +0,0 @@
-""" This is a Python Helper file that will be used to hand over career & education details to __init__.py """
---
+++ app/users.py
@@ -0,0 +1,275 @@
+team_projects = {
+    "project" : ['Team Portfolio Site',],
+    "project_lang" : ['Flask, JS, Jinja, Python, Leaflet',],
+    "project_date" : ['Jan 26/2023 - Feb 06/2023'],
+    "project_desc": {
+     0: ['Created a Portfolio Website as a team of 4, using Flask for the backend',"Javascript was used to add an interactive map using the Leaflet library to showcase members travel experience", 'Project was developed in an agile style, with members completing tasks using github issues to track progress and commits','Future plans involve deploying the project using a cloud provider', ],
+    },
+    "site_url": [''],
+    "github_url": ['https://github.com/MLH-Fellowship/project-vitrina'],
+}
+
+
+xavier_about = {
+    "contact": "xavierjmoreno401@gmail.com",
+    "aboutme": "I'm a Computer Engineering student who's very passionate about Robotics and AI both in a personal and professional avenue. As a result I'm starting to look deeper into topics like Machine Learning, Autonomous Vehicles and Humanoid Robotics. Currently I'm starting to focus on growing my skills in C++, Python, Operating Systems and Data Structures and Algorithms. I have a background in IT, which led to me gaining interest in Cloud Computing as well. On my personal interests, I absolutely love hackathons, they are great way to meet others and to explore new topics and push boundaries. As a result of them, I've started to become interested in joining both Kaggle Competitions and Robotics Tournaments."
+}
+
+""" Section fillouts """
+
+
+xavier_career = {
+    "jobcount": 4,
+    "companies": ["MLH Fellowship", "Southwest Airlines", "Fujitsu",  "Self-Employed"],
+    "jobtitle": ["Site Reliability Engineer Fellow",  "Noc Analyst", "Service Desk Analyst",  "Private Tutor"],
+    "dates": ["Jan 2023 - April 2023", "Nov 2022 - Present", "March 2021 - Nov 2021", "Nov 2019 - Present"],
+    "descriptions": {
+        0: ["Worked in a cohort of 20 other members working on various Open Source Projects in Siter Reliability Engineering",
+            "Projects ranged from creating a personal portfolio website to working on containers, Unit-Testing, CI/CD, monitoring and networking."],
+        1: ["Performed continuous monitoring & incident response of various systems through Grafana, escalating tickets when required using ServiceNow. Coordinated with various teams from departments on incidents",
+            "Continuously updated & monitored 250 RHEL 7 machines and 500 Windows 10 machines",
+            "Performed hardware and software upgrades for machines running AVTEC software"],
+        2: ["Citrix-Systems used to virtualize workstation PC with tools such as Wireshark, Teamviewer & AWS voice",
+            "Collaborated in a team addressing 10-20 tickets daily & advising up to 20 clients daily",
+            "SOTI mobicontrol & TeamViewer - Remoted into Windows PCs dealing with malware cleanup, software installation, password-resets, network-connectivity. Used ServiceNow for ticketing",
+            "Handled Zebra device issues, involving incorrect network configurations with IP or staging",
+            "Working with networking hardware ( Cisco Switches & Routers ) to address issues with DHCP / DNS / IP"],
+        3: ["Tutored Undergraduate students in 1-on-1 sessions, using personally tailored study plans for Math and CS",
+            "For Math taught Calculus I-II, for CS taught various languages, with a focus on Java & JavaScript",
+            "Studied and debugged thousands of lines of code and gave students feedback on their work"],
+    }
+}
+
+xavier_hobby = {
+    "n_hobbies": 2,
+    "hobbies": ["Hiking", "Language Learning"],
+    "description": ["In my opinion hiking is one of the best things you can do to relax yourself, especially when you are high on stress. Pictured to the right is a photo I took while on a hike in the Grand Canyon. Most of the hiking I do isn't as crazy as that, although my trip was a good memory. Generally just going walking among grassy and slightly rocky fields is my go to!", "I find studying languages to be very fascinating. There have been many studies done on the effects on language, and the results are very interesting. To study language is to quite literally change your brain and perspective. Well benefits aside, I simply enjoy taking on different systems of writing/speaking and thought. I'm currently studying Japanese, and so far it's been quite a journey. Kanji is definitely no joke!"],
+    "url": ['./static/img/xav_hobby/xav_hobby1.jpg', './static/img/xav_hobby/xav_hobby2.jpg'],
+    "credit": ['', 'credit :aaron-burden unsplash ']
+}
+
+xavier_mapper = {
+    "country_origin": "United States of America, TX, Dallas",
+    "hometown": "Dallas, TX",
+    "homecity": "Dallas",
+    "hometown_coords": {"lat": 32.7767, "long": -96.7970},
+    "places": ['Phoenix, AZ - Grand Canyon', 'Austin, TX - UT', 'San Antonio,  TX - UTSA', 'San Diego, CA - MCRD', 'Cambridge, MA - MIT', 'Cambridge, MA - Harvard', 'Stanford, CA - Stanford'],
+    "description": ['My visit to the grand canyon is easily one of my favorite memories. Enjoying time with friends in new places is always welcome.', 'My first time going to another city for a hackathon. This is where I participated at HackTX!', 'After HackTX in Austin, I ended up starting to travel further and further for hackathons. This is RowdyHacks.', 'This the first time I ever flew within the US. I saw one of my close relatives graduate at San Diego\'s Marine Bootcamp', 'I participated at HackMIT here. It was a legendary experience, and my first time flying for a hackathon!', 'In Boston again! This time as a Judge for HackHarvard.', 'One of the most exciting hackathons yet. Stanford\'s TreeHacks!'],
+    "places_coords": [
+        {"lat": 36.0544, "long": -112.1401},
+        {"lat": 30.2849, "long": -97.7341},
+        {"lat": 29.5831, "long": -98.6199},
+        {"lat": 32.7415, "long": -117.1977},
+        {"lat": 42.3601, "long": -71.0942},
+        {"lat": 42.3770, "long": -71.1167},
+        {"lat": 37.4275, "long": -122.1697}
+    ],
+}
+
+xavier_education = {
+    "university": "Dallas County Community College",
+    "degree": "Associates of Science, Computer Engineering",
+    "length": "Jan 2023 - December 2023"
+}
+
+""" Santiago section_info """
+
+santiago_about = {
+    "contact": "santiveraespinoza@gmail.com",
+    "aboutme": "I am a future engineer in computer technologies currently studying at Tecnológico de Monterrey (ITESM, 2021 - 2025). I have been programming since 16, self-taught. My main language is C++, I can also program in Python, Matlab, R and HTML. Made some videogames on my own, as well as some physics simulators. I also have good knowledge in Office and Adobe apps."
+}
+
+santiago_career = {
+    "jobcount": 1,
+    "companies": ["MLH Fellowship"],
+    "jobtitle": ["Site Reliability Engineer Fellow"],
+    "dates": ["Jan 2023 - April 2023"],
+    "descriptions": {
+        0: ["Worked in a cohort of 20 other members"],
+    }
+}
+
+santiago_hobby = {
+    "n_hobbies": 3,
+    "hobbies": ["Videogames", "Anime", "Soccer"],
+    "description": ["I love videogames with all my soul, they have been my passion since I was little. I love all its edges, from the art design to the gameplay generation. My favorites are from Nintendo, but I play everything. Video games will be something that even though the years go by I will continue playing.",
+                    "Anime is something that my brothers have included in me since I was a child, so I have normalized it a lot in my life. It is something super natural for me and away from the taboo, it is a very consumed product in my family. There are animes that have marked me and I consider they have many brilliant proposals for the world of entertainment. I also think that they have influenced a lot from the culture of western animation.",
+                    "Soccer is something that my family has instilled in me since I was a child. My uncles are very fanatical and that fever has infected me all my life. I have a great passion for my Leon team, and I always watch my team's games and playoffs."],
+    "url": ['./static/img/san_hobby/Videogames.jpg', './static/img/san_hobby/Anime.jpg', './static/img/san_hobby/Soccer.jpeg'],
+    "credit": ['', 'credit :aaron-burden unsplash ']
+}
+
+santiago_mapper = {
+    "country_origin": "United States of Mexico, Guanajuato, León of the Aldama",
+    "hometown": "León, GTO, MEX",
+    "homecity": "León, ",
+    "hometown_coords": {"lat": 21.122002, "long": -101.683309},
+    "places": ['New York, NY, USA', 'Jackson, MS, USA', 'Los Angeles, CA, USA', 'San Francisco, CA, USA', 'Denver, CO, USA', 'Albuquerque, NM, USA', 'New Orleans, LA, USA', 'Orlando, FL, USA', 'Dalas, TX, USA', 'San Antonio, TX, USA', 'Corpus Christi, TX, USA', 'Gruene, TX, USA', 'Fredericksburg, TX, USA', 'Johnson City, TX, USA', 'Tapalpa, JC, MEX', 'Tequila, JC, MEX', 'Guanajuato, GTO, MEX', 'San Miguel de Allende, GTO, MEX', 'Los Cabos, BCS, MEX', 'Santiago de Querétaro, QRO, MEX', 'Puebla, PU, MEX', 'Puerto de Veracruz, VE, MEX', 'Ocampo, MI, MEX', 'Cancún, QR, MEX', 'Playa del Carmen, QR, MEX', 'Tulum, QR, MEX', 'Holbox, QR, MEX', 'Mexico City, CDMX, MEX', 'Kingston, JMA', 'George Town, Caiman Islands'],
+    "description": [
+                    'I went to Times Square, Rockefeller Center, Chinatown, Central Park, the Statue of Liberty and many other tourist destinations.',
+                    'I went to NorthPark mall and to visit some relatives in Yazoo.',
+                    'I went to Disneyland, Hollywood and to visit my great aunt.',
+                    'I went to the Golden Gate bridge, Lombard street and many other destinations.',
+                    'I went to some malls and went shopping with my family.',
+                    'I went to see several of my relatives.',
+                    'I went to eat Gumbo, Beignets, walk downtown, went to the French Quarter, etc.',
+                    'I went to disney world, universal studios and various malls.',
+                    'I went for a walk with my family, but it was so long ago that I don\'t remember.',
+                    'I went to the Riverwalk, The Alamo and downtown, in addition to eating and walking around.',
+                    'Went to see the statue of Selena and spend New Years with my sister-in-law\'s family',
+                    'I went to eat BBQ ribs and walk downtown.',
+                    'I went to see the city center and the parks.',
+                    'I went to see the light decorations in the city.',
+                    'I went to see the magical town and its surroundings.',
+                    'I went to see the tequila factories and the town center.',
+                    'I went to see the center and walk through the alleys, the cathedrals, the tunnels, the museums, the university and the nightlife of this world heritage site.',
+                    'I went to see the center, the restaurants, the cathedral and the city market.',
+                    'I went to see the beaches and the city center.',
+                    'I went to visit my girlfriend and to walk through the center of the city and its squares.',
+                    'I went to the pyramid of Cholula and to Angelopolis.',
+                    'I went for a walk through the city, the port and to see the base of San Juan de Ulua.',
+                    'I went to visit the monarch butterfly forest.',
+                    'I went to the beach and the hotel zone.',
+                    'I went to the beach and to fifth avenue.',
+                    'I went to the hotel zone to see several cenotes.',
+                    'I went for a walk in the center of the island and for a walk on the beach.',
+                    'I went to Polanco, the Zona Rosa, Reforma, the Chapultepec Park, the Soumaya Museum and many other destinations.',
+                    'I went downtown and to the golden beaches of the country.',
+                    'I went to see the surroundings, the houses and the public beaches.'
+                    ],
+    "places_coords": [
+        {"lat": 40.748561, "long": -73.985949},
+        {"lat": 32.307996, "long": -90.223068},
+        {"lat": 34.049038, "long": -118.245602},
+        {"lat": 37.789362, "long": -122.404815},
+        {"lat": 39.738387, "long": -104.932711},
+        {"lat": 35.091881, "long": -106.649710},
+        {"lat": 30.015087, "long": -90.074147},
+        {"lat": 28.523341, "long": -81.378381},
+        {"lat": 32.766328, "long": -96.797480},
+        {"lat": 29.419371, "long": -98.496523},
+        {"lat": 27.766592, "long": -97.427650},
+        {"lat": 29.738066, "long": -98.103406},
+        {"lat": 30.271858, "long": -98.873254},
+        {"lat": 30.276094, "long": -98.409432},
+        {"lat": 19.945191, "long": -103.758844},
+        {"lat": 20.884962, "long": -103.838157},
+        {"lat": 21.017413, "long": -101.257367},
+        {"lat": 20.913694, "long": -100.743839},
+        {"lat": 22.889425, "long": -109.913515},
+        {"lat": 20.593162, "long": -100.386702},
+        {"lat": 19.042306, "long": -98.214254},
+        {"lat": 19.201246, "long": -96.143635},
+        {"lat": 19.561673, "long": -100.296288},
+        {"lat": 21.168719, "long": -86.839966},
+        {"lat": 20.637669, "long": -87.062793},
+        {"lat": 20.209861, "long": -87.465696},
+        {"lat": 21.523641, "long": -87.379452},
+        {"lat": 19.432471, "long": -99.130907},
+        {"lat": 18.017617, "long": -76.810133},
+        {"lat": 19.286895, "long": -81.367687}
+    ],
+}
+
+santiago_education = {
+    "university": "Instituto Tecnológico de Estudios Superiores de Monterrey",
+    "degree": "Bachelor in Computer Tecnologies Engineering",
+    "length": "Aug 2021 - Jun 2025 (Graduation date)"
+}
+
+""" Cindy section_info """
+
+cindy_about = {
+    "contact": "cindyliang0127@gmail.com",
+    "aboutme": "I am currently a junior studying Computer Science at NYU. I am passionate about full stack software development and in my free time, I like to immerse myself with Natural Langauge Processing and UX/UI design. I am currently honing my coding skills for C and would like to pick up more languages in the future so I can keeping learning and improving."
+}
+
+cindy_career = {
+    "jobcount": 4,
+    "companies": ["MLH Fellowship", "Self-Employed", "ReachNBeyond", "Naomedical"],
+    "jobtitle": ["Site Reliability Engineer Fellow", "Private Math Tutor", "Computer Science Teacher", "UX/UI Intern"],
+    "dates": ["Jan 2023 - April 2023", "January 2023 - Present", "June 2022 - Present", "June 2021 - August 2021"],
+    "descriptions": {
+        0: ["Worked in a cohort of 20 other members"],
+        1: ["Tutored K-12 students in 1-on-1 sessions with custom made lesson plans"],
+        2: ["Taught over 50 students Computer Science (Javascript) and Math"],
+        3: ["Designed app features to improve urgent health care user’s experience by making it more efficient and accessible. Experience working with tech stacks and startup operation"],
+    }
+}
+
+cindy_hobby = {
+    "hobbies": ["Dancing and Karaoke", "Watching Anime"],
+    "n_hobbies": 2,
+    "description": ["I love to learn hip-hop dances and sing off-pitch into my karaoke mic. I still don't know how to sing with my diaphram. Any advice would be much appreciated.", "I love to watch shounen anime (My favorite is One Piece)"],
+    "url": ['./static/img/cin_hobby/cin_hobby1.jpg', './static/img/cin_hobby/cin_hobby2.jpg'],
+    "credit": ['', 'credit :aaron-burden unsplash ']
+}
+
+cindy_mapper = {
+
+    "country_origin": "United States of America, NY, Brooklyn",
+    "hometown": "Brooklyn, NY",
+    "homecity": "Brooklyn",
+    "hometown_coords": {"lat": 40.650002, "long": -73.949997},
+    "places": ['New Haven, CT'],
+    "description": ['I rubbed the Therodore Dwight Woolsey statue for good luck. :)'],
+    "places_coords": [
+        {"lat": 41.316307, "long": -72.922585}
+    ]
+
+}
+
+cindy_education = {
+    "university": "New York University",
+    "degree": "Computer Science, Bachelor's Degree of Science",
+    "length": "September 2020 - May 2024"
+}
+
+
+""" Raven section_info """
+
+raven_about = {
+    "contact": "raejdickerson@gmail.com",
+    "aboutme": "I am a Junior Software Engineer with interests in DevOps/SRE and I love coding. When I'm not writing code, I love to dance and collect Sailor Moon merchandise. I am constantly looking to improve my technical skills and stay up-to-date with the latest trends in the industry."
+}
+
+raven_career = {
+    "jobcount": 3,
+    "companies": ["MLH Fellowship", "Hexagon", "Jack Henry & Associates", "Ignite Fueling Innovation"],
+    "jobtitle": ["Site Reliability Engineer Fellow", "Jr. Software Engineer", "DevOps Apprentice", "Help Desk Specialist"],
+    "dates": ["Jan 2023 - April 2023", "February 2023 - Present", "July 2022 - January 2023", "November 2021 - July 2022"],
+    "descriptions": {
+        0: ["Worked in a cohort of 20 other members"],
+        1: ["Designs, develops and maintains software"],
+        2: ["Was responsible for assisting in the automation and continuous delivery of software applications"],
+        3: ["Provided technical support to users by answering inquiries, resolving problems, and maintaining customer satisfaction with the use of computer systems and software"],
+    }
+}
+
+raven_hobby = {
+    "hobbies": ["Coding", "Dancing", "Collecting Sailor Moon Merch"],
+    "description": ["I love to code, dance & obsess over Sailor Moon"],
+    "url": ['./static/img/rav_hobby/code.jpg', './static/img/rav_hobby/dance.jpg', './static/img/rav_hobby/sailorMoon.jpg'],
+    "credit": ['', '', ''],
+    "n_hobbies": 3
+}
+
+raven_mapper = {
+    "country_origin": "United States of America, Baton Rouge, LA",
+    "hometown": "Baton Rouge, LA",
+    "homecity": "Baton Rouge",
+    "hometown_coords": {"lat": 30.471165, "long": -91.147385},
+    "places": ['Washington DC', 'Huntsville, AL'],
+    "description": ['I was at the Inauguration when former President Barack Obama was sworn in office!'],
+    "places_coords": [
+        {"lat": 38.9072, "long": 77.0369},
+        {"lat": 34.7304, "long": 86.5861}
+    ],
+}
+
+raven_education = {
+    "university": "JF Drake State Community & Technical College",
+    "degree": "Computer InformatSystems, Associates of ApplScience",
+    "length": "August 2019 - May 2021"
+}
+# {"university": "Western Governors University","degree": "Cloud Computing, Bachelor's Degree of Science","length": "July 2022 - December 2023"}
