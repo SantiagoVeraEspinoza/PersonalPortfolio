3ee86979aa1295ceb50026e62ea9a7a8091ee492
TitanPro47 <67120257+TitanPro47@users.noreply.github.com>
2023-02-22 18:25:34 -0600

    Create curl-test.sh

---
+++ curl-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+email=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)"@"$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)".com"
+content=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+
+curl --request POST http://10.43.52.207:5000/api/timeline_post -d "name=$name&email=$email&content=$content"
+
+curl http://10.43.52.207:5000/api/timeline_post | grep "$name" | grep "$email" | grep "$content"
