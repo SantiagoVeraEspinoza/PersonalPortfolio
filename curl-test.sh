a942ed75510b04c7086d5e2de01f5f398ebcf385
TitanPro47 <santiveraespinoza@gmail.com>
2023-02-22 22:41:17 -0600

    1.0.5

--- curl-test.sh
+++ curl-test.sh
@@ -4,6 +4,6 @@ name=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
 email=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)"@"$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)".com"
 content=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
 
-curl --request POST http://127.0.0.1:5000/api/timeline_post -d "name=$name&email=$email&content=$content"
+curl --request POST http://137.184.73.24:5000/api/timeline_post -d "name=$name&email=$email&content=$content"
 
-curl http://127.0.0.1:5000/api/timeline_post | grep "$name" | grep "$email" | grep "$content"
+curl http://137.184.73.24:5000/api/timeline_post | grep "$name" | grep "$email" | grep "$content"
