40d191e6b9463927a5b223cfbfbc27f650358b12
TitanPro47 <santiveraespinoza@gmail.com>
2023-03-14 18:47:30 -0600

    prueba 2

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -7,7 +7,7 @@ cd /root/Projects/MLH-project-vitrina-portfolio
 git fetch
 git reset origin/main --hard
 
-docker compose -f docker-compose.prod.yaml down --rmi all
+sudo docker compose -f docker-compose.prod.yaml down --rmi all
 docker compose -f docker-compose.prod.yaml up -d
 
 cd $WD
