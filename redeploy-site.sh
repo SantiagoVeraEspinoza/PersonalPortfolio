ab962f5920af9a33dee1cda9361a96caa6f525d0
Santiago Vera Espinoza <santiveraespinoza@gmail.com>
2023-08-03 19:20:20 -0600

    1.2.1

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -9,6 +9,6 @@ git reset origin/main --hard
 
 docker compose -f docker-compose.prod.yaml down
 docker compose -f docker-compose.prod.yaml up -d --build
-docker system prune
+docker system prune -f
 
 cd $WD
